3467ac3e13624b5fd034f74a4126fbe7f2a4f979
Chris Norman <chris.norman1989@outlook.com>
2024-11-25 16:23:32 +0000

    Only force semantics on web.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -15,7 +16,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(final BuildContext context) {
-    RendererBinding.instance.ensureSemantics();
+    if (kIsWeb) {
+      WidgetsFlutterBinding.ensureInitialized();
+      RendererBinding.instance.ensureSemantics();
+    }
     return MaterialApp(
       title: 'Presets',
       theme: ThemeData(
